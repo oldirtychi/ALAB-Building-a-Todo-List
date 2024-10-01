d157f4b4ebecd3d350e17c8c0119baf7e62e9cb5
oldirtychi <ejsapayanp@gmail.com>
2024-10-01 00:29:43 -0500

    delete task function

--- React-Todo-List/src/App.jsx
+++ React-Todo-List/src/App.jsx
@@ -4,7 +4,15 @@ const initialState = [];
 
 function reducer(state, action) {
     switch(action.type){
-        case 'Add_Task':
+        case 'Add_Task': return [
+            ...state,
+            {
+                id:state.length +1,
+                name: action.payload
+            }
+        ];
+
+        case 'Delete_Task': return state.filter(d => d.id !==action.payload)
 
         default: return state;
     }
@@ -14,10 +22,23 @@ const Todos = () => {
     const [todos, dispatch] = useReducer(reducer, initialState);
 
     return(
-        <>
+        <div style = {{ textAlign: "center"}}>
          <h1>My Todo List ({todos.length})</h1>
-         <input type="text" onBlur={() => dispatch({type: 'Add_Task'})}/>
-        </>
+        Add New Task:
+         <input type="text" 
+         onBlur={(e) => dispatch(
+            {type: 'Add_Task', payload: e.target.value}
+            )} /> 
+
+           {todos.map(todo => <li key={todo.id}>{todo.name}
+            <span>
+                <button onClick={() => dispatch(
+                    { type: 'Delete_Task', payload:todo.id})}>
+                    Delete
+                </button>
+            </span>
+           </li>)}
+        </div>
     )
 }
 
