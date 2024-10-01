bab1a66ada5b015d2f831092bf0a9c618ae0d59f
oldirtychi <ejsapayanp@gmail.com>
2024-10-01 00:00:35 -0500

    add task function

--- React-Todo-List/src/App.jsx
+++ React-Todo-List/src/App.jsx
@@ -1,2 +1,24 @@
 import {useReducer} from 'react';
 
+const initialState = [];
+
+function reducer(state, action) {
+    switch(action.type){
+        case 'Add_Task':
+
+        default: return state;
+    }
+}
+
+const Todos = () => {
+    const [todos, dispatch] = useReducer(reducer, initialState);
+
+    return(
+        <>
+         <h1>My Todo List ({todos.length})</h1>
+         <input type="text" onBlur={() => dispatch({type: 'Add_Task'})}/>
+        </>
+    )
+}
+
+export default Todos;
